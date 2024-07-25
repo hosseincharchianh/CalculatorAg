011ec1efaea72dfa5251a35dc5b21e09cb689a9a
hossein charchian <140173142+hosseincharchianh@users.noreply.github.com>
2024-07-25 17:44:19 +0330

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalcComponent } from "./calc/calc.component";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalcComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calc',
+  standalone: true,
+  imports: [],
+  templateUrl: './calc.component.html',
+  styleUrl: './calc.component.css'
+})
+export class CalcComponent {
+
+}
