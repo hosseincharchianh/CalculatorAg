0196d5bcfd9e0d4ce954fb5661f06334b922a220
hossein charchian <140173142+hosseincharchianh@users.noreply.github.com>
2024-07-28 22:21:56 +0330

    add semicolon

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -9,11 +9,11 @@ import { Component, numberAttribute } from '@angular/core';
 })
 export class CalcComponent {
 delAll() {
-  this.firstNumber='0'  
+  this.firstNumber='0'  ;
 }
-  firstNumber: string = '0'
-  secondNumber: string = '0'
-  operator: string = ''
+  firstNumber: string = '0';
+  secondNumber: string = '0';
+  operator: string = '';
   button(d: string) {
     // if (this.firstNumber = 'Enter first Number :') {
     //   this.firstNumber = ''
@@ -21,45 +21,45 @@ delAll() {
     this.firstNumber = Number(this.firstNumber + d).toString()
   }
   operatorF(d: string) {
-    this.operator = d
-    this.secondNumber = this.firstNumber
-    this.firstNumber = '0'
+    this.operator = d;
+    this.secondNumber = this.firstNumber;
+    this.firstNumber = '0';
   }
   ac() {
-    this.firstNumber = '0'
-    this.secondNumber = '0'
-    this.operator = ''
+    this.firstNumber = '0';
+    this.secondNumber = '0';
+    this.operator = '';
   }
 
   //inja ye bug hast ...... vaghti hameye mohtavaye #FN ro pak mikonim mipare balaye #SN 😂
   del() {
-    let fn: string = this.firstNumber
-    let main: string = fn.slice(0, fn.length - 1)
-    this.firstNumber = main
+    let fn: string = this.firstNumber;
+    let main: string = fn.slice(0, fn.length - 1);
+    this.firstNumber = main;
     if(this.firstNumber=='')
     {
-      this.firstNumber='0'
+      this.firstNumber='0';
     }
   }
   equal() {
     if(this.operator=='+')
     {
-      this.firstNumber=(Number(this.firstNumber)+Number(this.secondNumber)).toString()
+      this.firstNumber=(Number(this.firstNumber)+Number(this.secondNumber)).toString();
     }
     else if(this.operator='*')
     {
-      this.firstNumber=(Number(this.firstNumber)*Number(this.secondNumber)).toString()
+      this.firstNumber=(Number(this.firstNumber)*Number(this.secondNumber)).toString();
     }
     else if(this.operator='/')
     {
-      this.firstNumber=(Number(this.firstNumber)/Number(this.secondNumber)).toString()
+      this.firstNumber=(Number(this.firstNumber)/Number(this.secondNumber)).toString();
     }
     else if(this.operator='-')
     {
-      this.firstNumber=(Number(this.firstNumber)-Number(this.secondNumber)).toString()
+      this.firstNumber=(Number(this.firstNumber)-Number(this.secondNumber)).toString();
     }
     this.secondNumber='0';
-    this.operator=''
+    this.operator='';
   }
 
 }
